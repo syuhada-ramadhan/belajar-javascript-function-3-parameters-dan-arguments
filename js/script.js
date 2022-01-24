c2bfbcce34ea4d87b2b82dc2974d29c7d730ef6b
syuhada-ramadhan <suhadahada18@gmail.com>
2022-01-25 06:34:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function keuntungan(hargaJual, hargaBeli, hutang) {
+  console.log(`Harga Penjualan: ${hargaJual}`);
+  console.log(`Harga Pembelian: ${hargaBeli}`);
+  console.log(`Hutang: ${hutang}`);
+}
+
+const keuntunganJualKambing = keuntungan(3000, 2000, 0, 500);
